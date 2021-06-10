ec79108f5ad6e01190ec037dcfcad17c430777d7
A601-12 <tnk.dat.class@gmail.com>
2021-06-10 09:44:03 +0900

    a

--- fukuvHensu/Form1.cs
+++ fukuvHensu/Form1.cs
@@ -19,8 +19,8 @@ namespace fukuvHensu
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left = int.Parse(textBox1.Text);
-            label1.Top = int.Parse(textBox2.Text);
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
         }
 
         private void button1_Click(object sender, EventArgs e)
